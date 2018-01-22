a2370b8672fbe299b619864e3d962c676c4b7819
Lee-Roy Gaseller <leeroygaseller@gmail.com>
2018-01-22 10:29:54 -0600

    Customized welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n\nHello, AttendanceApp!\n");
+
+        // Customized welcome message
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Welcome, " + name + "!");
     }
 }
